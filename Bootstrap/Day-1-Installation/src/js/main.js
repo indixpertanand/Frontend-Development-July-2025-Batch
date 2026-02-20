727fee415e6ab2c01a60464d01db528693161103
Anand Singh <anand.singh@indixpert.com>
2026-02-20 14:43:57 +0530

    Updating Bootstrap Project Footer

--- Bootstrap/Day-1-Installation/src/js/main.js
+++ Bootstrap/Day-1-Installation/src/js/main.js
@@ -2,3 +2,20 @@ import '../scss/styles.scss'
 
 
 import * as bootstrap from 'bootstrap'
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+// const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl, {container: '#popoverContainer'}))
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+    toastTrigger.addEventListener('click', () => {
+        toastBootstrap.show()
+    })
+}
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
